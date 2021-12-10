1ee1ddad206bf45ee3ae4f7f07f6a9f4995e3565
KieenVux <40339436+KieenVux@users.noreply.github.com>
2021-12-10 17:21:15 +0700

    change service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello buoi` dai 30cm!';
+    return 'Hello buoi` dai 40cm!';
   }
 }
